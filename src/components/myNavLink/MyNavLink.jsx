cdb4b147044fe33caf58b2213c1cdb5c82477193
Morshedul-developer <nijhumkhandoker9@gmail.com>
2026-06-15 12:19:21 +0600

    made the navbar responsive

--- src/components/myNavLink/MyNavLink.jsx
+++ src/components/myNavLink/MyNavLink.jsx
@@ -2,7 +2,7 @@ import { NavLink } from "react-router";
 
 const MyNavLink = ({ to, children }) => {
   return (
-    <NavLink className={({isActive})=>`${isActive ? 'bg-[#244D3F] text-white' : ''} flex items-center gap-1 btn`} to={to}>
+    <NavLink className={({isActive})=>`${isActive ? 'bg-[#244D3F] text-white' : ''} btn`} to={to}>
       {children}
     </NavLink>
   );
--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -1,26 +1,34 @@
 import { RiHome2Line, RiTimeLine } from "react-icons/ri";
 import { TfiStatsUp } from "react-icons/tfi";
 import MyNavLink from "../myNavLink/MyNavLink";
+import { TiThMenu } from "react-icons/ti";
 
 const NavBar = () => {
+  const navItems = [
+    { name: "Home", icon: <RiHome2Line size={16} />, path: "/" },
+    { name: "Timeline", icon: <RiTimeLine size={16} />, path: "/timeline" },
+    { name: "Stats", icon: <TfiStatsUp size={16} />, path: "/stats" },
+  ];
   return (
-    <nav className="bg-[#E5E7EB] shadow-md">
+    <nav className="shadow-md">
       <div className="flex items-center justify-between p-4 max-w-7xl mx-auto">
         <div>
           <h3 className="text-xl font-bold text-[#1F2937]">
             Keen<span className="font-semibold text-[#244D3F]">Keeper</span>
           </h3>
         </div>
-        <ul className="flex gap-4">
-          <li className="text-[#1F2937] hover:text-[#244D3F] cursor-pointer">
-            <MyNavLink to={"/"}><RiHome2Line size={16} />Home</MyNavLink>
-          </li>
-          <li className="text-[#1F2937] hover:text-[#244D3F] cursor-pointer flex items-center gap-1">
-            <MyNavLink to={"/timeline"}><RiTimeLine size={16} />Timeline</MyNavLink>
-          </li>
-          <li className="text-[#1F2937] hover:text-[#244D3F] cursor-pointer flex items-center gap-1">
-            <MyNavLink to={"/stats"}><TfiStatsUp size={16} />Stats</MyNavLink>
-          </li>
+        <div className="md:hidden">
+          <TiThMenu size={20} />
+        </div>
+        <ul className="gap-4 items-center hidden md:flex">
+          {navItems.map((item,index) => (
+            <li key={index}>
+              <MyNavLink to={item.path}>
+                {item.icon}
+                {item.name}
+              </MyNavLink>
+            </li>
+          ))}
         </ul>
       </div>
     </nav>
