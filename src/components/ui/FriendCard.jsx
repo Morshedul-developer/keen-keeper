828ac679059da3743578a86323b77d3158dca1c3
Morshedul-developer <nijhumkhandoker9@gmail.com>
2026-06-15 17:47:03 +0600

    completed the friend card designs

--- src/components/ui/FriendCard.jsx
+++ src/components/ui/FriendCard.jsx
@@ -1,6 +1,6 @@
 const FriendCard = ({ friend }) => {
   return (
-    <div className="bg-base-100 shadow-sm p-6 rounded-lg space-y-4">
+    <div className="bg-base-100 shadow-md p-6 rounded-lg space-y-4">
       <figure>
         <img
           src={friend.picture}
@@ -14,15 +14,18 @@ const FriendCard = ({ friend }) => {
           {friend.days_since_contact}d ago
         </p>
         <div className="flex flex-wrap justify-center gap-2">
-            {
-                friend.tags.map((tag, index) => (
-                    <span key={index} className="badge bg-[#CBFADB] text-[#244D3F] rounded-full uppercase font-medium text-xs">
-                        {tag}
-                    </span>
-                ))
-            }
+          {friend.tags.map((tag, index) => (
+            <span
+              key={index}
+              className="badge bg-[#CBFADB] text-[#244D3F] rounded-full uppercase font-medium text-xs"
+            >
+              {tag}
+            </span>
+          ))}
         </div>
-        <p className={`badge rounded-full capitalize font-medium text-xs text-white ${friend.status === 'overdue' ? 'bg-[#EF4444]' : friend.status === 'on-track' ? 'bg-[#244D3F]' : 'bg-[#EFAD44]' }`}>
+        <p
+          className={`badge rounded-full capitalize font-medium text-xs text-white ${friend.status === "overdue" ? "bg-[#EF4444]" : friend.status === "on-track" ? "bg-[#244D3F]" : "bg-[#EFAD44]"}`}
+        >
           {friend.status}
         </p>
       </div>
