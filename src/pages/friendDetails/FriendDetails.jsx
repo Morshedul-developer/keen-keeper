5d5a6a158d79aaa8c3d0c0e3db184e6037e49d43
Morshedul-developer <nijhumkhandoker9@gmail.com>
2026-06-16 12:52:26 +0600

    completed the friend details page

--- src/pages/friendDetails/FriendDetails.jsx
+++ src/pages/friendDetails/FriendDetails.jsx
@@ -1,6 +1,10 @@
 import { useParams } from "react-router";
 import useFriends from "../../hooks/useFriends";
 import { ClipLoader } from "react-spinners";
+import { RiDeleteBinLine, RiNotificationSnoozeLine } from "react-icons/ri";
+import { FiArchive } from "react-icons/fi";
+import { PiChatDotsBold, PiPhoneCallBold } from "react-icons/pi";
+import { LuVideo } from "react-icons/lu";
 
 const FriendDetails = () => {
   const { friendId } = useParams();
@@ -71,14 +75,46 @@ const FriendDetails = () => {
         <div className="col-span-9 row-span-2 shadow-sm rounded-lg p-6 space-y-2">
           <div className="flex justify-between items-center">
             <p className="text-xl font-semibold">Relationship Goal</p>
-            <button className="btn bg-[#F8FAFC] hover:bg-[#E2E8F0]">Edit</button>
+            <button className="btn bg-[#F8FAFC] hover:bg-[#E2E8F0]">
+              Edit
+            </button>
           </div>
-          <p className="text-xl text-[#64748B]">Connect every <span className="font-bold text-black">{expectedFriend?.goal} days</span></p>
+          <p className="text-xl text-[#64748B]">
+            Connect every{" "}
+            <span className="font-bold text-black">
+              {expectedFriend?.goal} days
+            </span>
+          </p>
         </div>
-        <div className="col-span-3 row-span-1 border p-5"></div>
-        <div className="col-span-9 row-span-3 border p-5"></div>
-        <div className="col-span-3 row-span-1 border p-5"></div>
-        <div className="col-span-3 row-span-1 border p-5"></div>
+        <button className="btn col-span-3 h-full row-span-1 text-[16px] bg-[#F8FAFC] hover:bg-[#E2E8F0] gap-2">
+          <RiNotificationSnoozeLine size={20} />
+          Snooze 2 weeks
+        </button>
+        <div className="col-span-9 row-span-3 shadow-sm rounded-lg p-6 space-y-4">
+          <p className="text-xl font-semibold">Quick Check-In</p>
+          <div className="grid grid-cols-3 gap-4">
+            <button className="btn h-32 flex-col bg-[#F8FAFC] hover:bg-[#E2E8F0] text-[16px] gap-2">
+              <PiPhoneCallBold size={30} />
+              Call
+            </button>
+            <button className="btn h-32 flex-col bg-[#F8FAFC] hover:bg-[#E2E8F0] text-[16px] gap-2">
+              <PiChatDotsBold size={30} />
+              Text
+            </button>
+            <button className="btn h-32 flex-col bg-[#F8FAFC] hover:bg-[#E2E8F0] text-[16px] gap-2">
+              <LuVideo size={30} />
+              Video
+            </button>
+          </div>
+        </div>
+        <button className="btn col-span-3 h-full row-span-1 text-[16px] bg-[#F8FAFC] hover:bg-[#E2E8F0] gap-2">
+          <FiArchive size={17} />
+          Archive
+        </button>
+        <button className="btn col-span-3 h-full row-span-1 text-[16px] bg-[#F8FAFC] hover:bg-[#E2E8F0] gap-2">
+          <RiDeleteBinLine size={20} />
+          Delete
+        </button>
       </div>
     </div>
   );
