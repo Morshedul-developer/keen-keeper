6b6edc633e555d53881e4ff08024ac83f3f7f85c
Morshedul-developer <nijhumkhandoker9@gmail.com>
2026-06-16 23:01:41 +0600

    fixed some bugs to the project

--- src/components/ui/TimelineCard.jsx
+++ src/components/ui/TimelineCard.jsx
@@ -1,22 +1,24 @@
-
 const TimelineCard = ({ fd }) => {
   return (
     <div className="p-4 shadow-sm rounded-lg flex items-center gap-4 bg-white">
       <div>
         {fd.event === "call" ? (
-          <img className="w-8" src="/call.png" alt={fd.name}/>
+          <img className="w-8" src="/call.png" alt={fd.name} />
         ) : fd.event === "text" ? (
-          <img className="w-8" src="/text.png" alt={fd.name}/>
+          <img className="w-8" src="/text.png" alt={fd.name} />
         ) : (
-          <img className="w-8" src="/video.png" alt={fd.name}/>
+          <img className="w-8" src="/video.png" alt={fd.name} />
         )}
       </div>
       <div>
         <p className="text-[#64748B]">
-          <span className="capitalize font-semibold text-[#244D3F]">{fd.event}</span> with {fd.name}
+          <span className="capitalize font-semibold text-[#244D3F]">
+            {fd.event}
+          </span>{" "}
+          with {fd.name}
         </p>
         <p className="text-[#64748B] text-sm font-semibold">
-          {new Date(fd.next_due_date).toLocaleDateString("en-US", {
+          {new Date().toLocaleDateString("en-US", {
             month: "short",
             day: "numeric",
             year: "numeric",
--- src/pages/timeLine/TimeLine.jsx
+++ src/pages/timeLine/TimeLine.jsx
@@ -3,19 +3,39 @@ import { FriendsContext } from "../../context/FriendContext";
 import TimelineCard from "../../components/ui/TimelineCard";
 
 const TimeLine = () => {
-  const { fnd } = useContext(FriendsContext);
+  const { fnd, setFnd } = useContext(FriendsContext);
+  const handleCall = () => {
+    const calledItems = fnd.filter((fd) => fd.event === "call");
+    setFnd(calledItems);
+  };
+  const handleText = () => {
+    const calledItems = fnd.filter((fd) => fd.event === "text");
+    setFnd(calledItems);
+  };
+  const handleVideo = () => {
+    const calledItems = fnd.filter((fd) => fd.event === "video");
+    setFnd(calledItems);
+  };
   return (
     <div className="bg-[#F8FAFC] py-10 md:py-20">
-      <div className="max-w-5xl mx-auto space-y-6">
+      <div className="max-w-5xl mx-auto space-y-6 px-4 md:px-0">
         <h2 className="text-4xl font-bold">Timeline</h2>
-        <select defaultValue="Filter timeline" className="select appearance-none">
+        <select
+          defaultValue="Filter timeline"
+          className="select appearance-none"
+          onChange={(e) => {
+            if (e.target.value === "Call") handleCall();
+            else if (e.target.value === "Text") handleText();
+            else if (e.target.value === "Video") handleVideo();
+          }}
+        >
           <option disabled={true}>Filter timeline</option>
           <option>Call</option>
           <option>Text</option>
           <option>Video</option>
         </select>
-        {fnd.map((fd) => (
-          <TimelineCard key={fd.id} fd={fd}/>
+        {fnd.map((fd, index) => (
+          <TimelineCard key={index} fd={fd} />
         ))}
       </div>
     </div>
