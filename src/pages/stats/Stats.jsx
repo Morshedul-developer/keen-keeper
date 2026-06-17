fec01358ffb3ec4f317f22122ca99eb2fef03922
Morshedul-developer <nijhumkhandoker9@gmail.com>
2026-06-17 12:53:03 +0600

    complete the layout design of stats page

--- src/pages/stats/Stats.jsx
+++ src/pages/stats/Stats.jsx
@@ -1,10 +1,11 @@
-
 const Stats = () => {
-    return (
-        <div>
-            Stats
-        </div>
-    );
+  return (
+    <div className="bg-[#F8FAFC] py-10 md:py-20">
+      <div className="max-w-5xl mx-auto space-y-6 px-4 md:px-0">
+        Stats
+      </div>
+    </div>
+  );
 };
 
 export default Stats;
