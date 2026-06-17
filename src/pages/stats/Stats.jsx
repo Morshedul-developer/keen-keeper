024e8fec4075cf8d69cbfe4aca176f7a5249deb2
Morshedul-developer <nijhumkhandoker9@gmail.com>
2026-06-17 15:47:58 +0600

    completed the stats page functionality

--- src/pages/stats/Stats.jsx
+++ src/pages/stats/Stats.jsx
@@ -1,40 +1,50 @@
-import { Pie, PieChart } from "recharts";
+import { Legend, Pie, PieChart, Tooltip } from "recharts";
+import { useContext } from "react";
+import { FriendsContext } from "../../context/FriendContext";
 
 const Stats = () => {
+  const { fnd } = useContext(FriendsContext);
+  const callLength = fnd.filter((fd) => fd.event === "call").length;
+  const textLength = fnd.filter((fd) => fd.event === "text").length;
+  const videoLength = fnd.filter((fd) => fd.event === "video").length;
   const data = [
-    { name: "Group A", value: 400, fill: "#0088FE" },
-    { name: "Group B", value: 300, fill: "#00C49F" },
-    { name: "Group C", value: 300, fill: "#FFBB28" },
-    { name: "Group D", value: 200, fill: "#FF8042" },
+    { name: "Text", value: callLength, fill: "#7E35E1" },
+    { name: "Call", value: textLength, fill: "#244D3F" },
+    { name: "Video", value: videoLength, fill: "#37A163" },
   ];
   return (
-    <div className="bg-[#F8FAFC] py-10 md:py-20">
+    <div className="bg-[#F8FAFC] py-10 md:py-18">
       <div className="max-w-5xl mx-auto space-y-6 px-4 md:px-0">
         <h2 className="text-4xl font-bold">Friendship Analytics</h2>
         <div className="p-8 rounded-lg shadow-sm bg-white space-y-6">
-            <h4 className="text-[20px] font-medium text-[#244D3F]">By Interaction Type</h4>
-            <PieChart
-          style={{
-            width: "100%",
-            maxWidth: "500px",
-            maxHeight: "80vh",
-            aspectRatio: 1,
-          }}
-          responsive
-        >
-          <Pie
-            data={data}
-            innerRadius="80%"
-            outerRadius="100%"
-            // Corner radius is the rounded edge of each pie slice
-            cornerRadius="50%"
-            fill="#8884d8"
-            // padding angle is the gap between each pie slice
-            paddingAngle={5}
-            dataKey="value"
-            isAnimationActive={true}
-          />
-        </PieChart>
+          <h4 className="text-[20px] font-medium text-[#244D3F]">
+            By Interaction Type
+          </h4>
+          <PieChart
+            style={{
+              width: "100%",
+              maxWidth: "350px",
+              maxHeight: "80vh",
+              aspectRatio: 1,
+              margin: "auto",
+            }}
+            responsive
+          >
+            <Pie
+              data={data}
+              innerRadius="80%"
+              outerRadius="100%"
+              // Corner radius is the rounded edge of each pie slice
+              cornerRadius="50%"
+              fill="#8884d8"
+              // padding angle is the gap between each pie slice
+              paddingAngle={5}
+              dataKey="value"
+              isAnimationActive={true}
+            />
+            <Legend />
+            <Tooltip />
+          </PieChart>
         </div>
       </div>
     </div>
